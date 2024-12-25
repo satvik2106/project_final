4b73d032dc3de6f194995eb6a19db371df409dee
satvik2106 <satvik.vattipalli1311@gmail.com>
2024-12-25 23:02:58 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:3000',              // Local development frontend
   'https://signare-g182.vercel.app', // Deployed Vercel frontend
-  'https://signare-g-182.vercel.app', // Deployed Vercel frontend
+  'https://signare-g182.netlify.app', // Deployed Vercel frontend
 ];
 
 // Enable CORS
