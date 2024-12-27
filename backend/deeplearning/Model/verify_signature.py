fa270890f2e8a4a9cc62995ae34dcf4fba34bd64
satvik2106 <satvik.vattipalli1311@gmail.com>
2024-12-27 19:37:12 +0530

    update

--- backend/deeplearning/Model/verify_signature.py
+++ backend/deeplearning/Model/verify_signature.py
@@ -91,5 +91,7 @@ def verify_signature():
     except Exception as e:
         return jsonify({"error": f"An unexpected error occurred: {str(e)}"}), 500
 
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+if __name__ == "__main__":
+    # Get the port from the environment variable or default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)  # Set debug=True only for development
