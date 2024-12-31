2ccd406747bf0613e089628c64248994a36a617b
satvik2106 <satvik.vattipalli1311@gmail.com>
2024-12-31 17:20:03 +0530

    Delete backend directory

--- backend/models/Signature.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const SignatureSchema = new mongoose.Schema({
-    accountNumber: { type: String, required: true },
-    signature: { type: String, required: true }, // Path or Base64 of the signature
-});
-
-module.exports = mongoose.model('Signature', SignatureSchema);
